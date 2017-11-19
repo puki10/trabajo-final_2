d52a792dd0a4259696be67a1644f5c5e7d256548
puki10 <30710348+puki10@users.noreply.github.com>
2017-11-19 15:40:33 -0600

    Add files via upload

---
+++ js/accesible.js
@@ -0,0 +1,11 @@
+var acceso = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 1, "Id": 0.0, "NEAR_FID": 14.0, "NEAR_DIST": 42.5324476515, "calle": "vecinal", "idw": 1 }, "geometry": { "type": "Point", "coordinates": [ -84.17561039452535, 9.939147286120861 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 2, "Id": 0.0, "NEAR_FID": -1.0, "NEAR_DIST": -1.0, "calle": "terciaria", "idw": 2 }, "geometry": { "type": "Point", "coordinates": [ -84.174606702036883, 9.892872135020644 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 3, "Id": 0.0, "NEAR_FID": 4.0, "NEAR_DIST": 49.871689488800001, "calle": "secundaria", "idw": 3 }, "geometry": { "type": "Point", "coordinates": [ -84.169176700724293, 9.9316146573588 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 4, "Id": 0.0, "NEAR_FID": 4.0, "NEAR_DIST": 40.8058593696, "calle": "secundaria", "idw": 3 }, "geometry": { "type": "Point", "coordinates": [ -84.168997369700421, 9.931763048834412 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 5, "Id": 0.0, "NEAR_FID": 0.0, "NEAR_DIST": 0.0, "calle": "vecinal", "idw": 1 }, "geometry": { "type": "Point", "coordinates": [ -84.199283441316567, 9.951860290339742 ] } }
+]
+}
